7f55a7f6a7cede096e7c5bd8e8326876b1658580
wnico-tpx240 <wnico233@outlook.com>
2023-02-27 19:50:14 +0800

    lab0 exer2 upd1

--- ray/sh_test1/changefile.sh
+++ ray/sh_test1/changefile.sh
@@ -4,10 +4,10 @@ while [ $a -le 100 ]
 do
 	if [ $a -gt 70 ]	   #if loop variable is greater than 70
 	then
-		rm "file$a"
+		rm -r "./file$a"
 	elif [ $a -gt 40 ]         # else if loop variable is great than 40
 	then
 		mv "file$a" "newfile$a"
 	fi
-	           a=$a + 1
+	a=$((a+1))
 done
