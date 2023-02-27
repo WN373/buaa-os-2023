16faa4cc460ea271e1005b8c9162c0f1cc958c35
wnico-tpx240 <wnico233@outlook.com>
2023-02-27 19:35:29 +0800

    lab0 exer1 update2

--- src/palindrome.c
+++ src/palindrome.c
@@ -2,14 +2,14 @@
 int main() {
 	int n;
 	scanf("%d", &n);
-	int s[5];
+	int s[6];
 	int l = 0;
 	for (; n > 0; l++) {
 		s[l] = n % 10;
 		n /= 10;
 	}
-	for (int i = 0; i <= l; i++) {
-		if (s[i] != s[l-i]) {
+	for (int i = 0; i < l; i++) {
+		if (s[i] != s[l-i-1]) {
 			printf("N");
 			return 0;
 		}
