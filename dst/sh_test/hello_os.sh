1f8819d1a2909a271d8049063ed72d767238044e
Xjchen <wnico233@outlook.com>
2023-03-06 15:26:13 +0800

    commited lab0-helper

--- dst/sh_test/hello_os.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-touch $2
-sed -n '8p' -n $1 >> $2
-sed -n '32p' -n $1 >> $2
-sed -n '128p' -n $1 >> $2
-sed -n '512p' -n $1 >> $2
-sed -n '1024p' -n $1 >> $2
--- ray/sh_test2/search.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#First you can use grep (-n) to find the number of lines of string.
-#Then you can use awk to separate the answer.
-grep -i -n  $2 $1 > middle 
-awk -F: '{print $1}' middle > $3
-rm middle
